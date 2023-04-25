8a30cd2de9cfbb01ee7750eedf6224c2704cc1eb
Denis Petrenko <denis.petrenko.dp@gmail.com>
2023-04-25 15:58:55 +0300

    add inference.py

---
+++ src/agbmfc/inference.py
@@ -0,0 +1,37 @@
+import numpy as np
+import onnxruntime
+
+_ORT_SESSION = onnxruntime.InferenceSession(r"../models/baseline-model-05.onnx")
+
+
+def onnx_inference(chip_tensor: np.ndarray, expected_batch_size: int = 10_000):
+    # agbmfc.loading.chip_tensor_to_pixel_tensor
+    pixel_tensor = np.transpose(chip_tensor, (2, 3, 0, 1)).reshape([65536, 12, 11])
+
+    result = []
+    first = 0
+    last = expected_batch_size
+
+    while first <= len(pixel_tensor):
+        batch = pixel_tensor[first:last]
+
+        real_batch_size = len(batch)
+        padding_size = expected_batch_size - real_batch_size
+        padding = np.zeros((padding_size, 12, 11), dtype=np.float32)
+        batch = np.concatenate((batch, padding))
+        ort_inputs = {'pixel_tensor': batch}
+        batch_result = _ORT_SESSION.run(None, ort_inputs)[0]
+        #         batch_result = np.random.randint(low=0, high=255, size=expected_batch_size)
+        batch_result = batch_result[:real_batch_size] if padding_size else batch_result
+
+        #         debug
+        #         print(first, real_batch_size, len(batch_result))
+
+        result.append(batch_result)
+
+        first = last
+        last += expected_batch_size
+
+    result = np.concatenate(result, axis=None)
+
+    return result.reshape(256, 256)
